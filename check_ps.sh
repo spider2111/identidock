66733f4c899de3f9f5cbc10fa079c726a8fd9598
spider2111 <A.Nafashyan@servicetk.ru>
2024-01-22 16:15:30 +0300

    some changes

--- check_ps.sh
+++ check_ps.sh
@@ -5,17 +5,10 @@ set -e
 #### Проверка на наличие имеющихся контейнеров хосте
 
 
-#checker=$(docker ps)
-output="output.txt"
-#string=$(cat "$output" | grep -c identidock)
 
 
-touch output.txt
-checker=$(docker ps)
-echo "$checker" > "$output"
-string=$(cat "$output" | grep -c identidock)
-if [[ $string -gt 0  ]]; then
+checker="$(docker ps) | grep -c identidock"
+if [[ $checker -gt 0  ]]; then
     docker stop $(docker ps -aq)
     docker rm $(docker ps -aq)
-    rm -rf $output
 fi
