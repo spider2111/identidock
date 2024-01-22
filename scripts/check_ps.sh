a23dc0a8a26550e6a157d440d2352f59ec28a880
spider2111 <A.Nafashyan@servicetk.ru>
2024-01-22 16:11:20 +0300

    some changes

---
+++ scripts/check_ps.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e 
+
+#### Проверка на наличие имеющихся контейнеров хосте
+
+
+
+
+checker="$(docker ps) | grep -c identidock"
+if [[ $checker -gt 0  ]]; then
+    docker stop $(docker ps -aq)
+    docker rm $(docker ps -aq)
+fi
